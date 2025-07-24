24ed747299876f25f3f74b4dae112c2510ab1589
Malizma333 <malizma333@gmail.com>
2025-07-24 17:31:48 -0500

    readded js dprint and edit capture state

--- src/utils/capture_state.js
+++ src/utils/capture_state.js
@@ -1,15 +1,15 @@
 // Utility to download track state from linerider.com for test case usage
 
 function captureState(testName, includeScarf = false) {
-  const state = [];
+  const state = {};
   const index = Math.floor(store.getState().player.index);
-  state.push(testName);
-  state.push(index);
-  state.push(store.getState().trackData.label);
+  state["test"] = testName;
+  state["frame"] = index;
+  state["name"] = store.getState().trackData.label;
   const formatNumber = (n) => n.toPrecision(17).replace(/\.?0+$/, "");
   // peg, tail, nose, string, butt, shoulder, rhand, lhand, lfoot, rfoot, scarf0...6
-  state.push(
-    store.getState().simulator.engine.getFrame(index).snapshot.entities[0].entities.map(entity =>
+  state["state"] = {
+    "entity": store.getState().simulator.engine.getFrame(index).snapshot.entities[0].entities.map(entity =>
       entity.points.map(
         point => [
           formatNumber(point.pos.x),
@@ -19,11 +19,11 @@ function captureState(testName, includeScarf = false) {
         ],
       )
     ),
-  );
+  };
 
   if (!includeScarf) {
     for (let i = 0; i < state[3].length; i++) {
-      state[3][i].splice(10, 7)
+      state[3][i].splice(10, 7);
     }
   }
 
